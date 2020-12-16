a746c2e4dd6708eb07e720e3a53d1adf100d0c0e
chugandar <furqanar03@gmail.com>
2020-12-16 19:28:47 +0530

    First Commit

---
+++ server/db.js
@@ -0,0 +1,35 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+let instance = null;
+dotenv.config();
+const dbUsername = process.env.USERNAME;
+const dbHost = process.env.HOST;
+//const dbPassword = process.env.PASSWORD;
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
+});
+//console.log(dbUsername+" "+dbHost);
+
+connection.connect((err)=>{
+    if(err) console.log(err);
+    else console.log("DB Connected!");
+});
+
+class Db{
+    static getDbInstance(){
+        return instance ? instance : new Db();
+    }
+    async getAllData(){
+        try {
+            const response = await new Promise((resolve,reject)=>{
+                const query = "SELECT * FROM "
+            });
+        } catch (error) {
+            
+        }
+    }
+}
